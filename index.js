9883e317450c646eaf8f8893bd1d7eb51e780285
Matt Wynne <matt@mattwynne.net>
2016-09-14 13:38:47 +0100

    More concrete API, including a way to look up from role -> person

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
+function getPersonNamed(book, name) {
+  article = getArticleByTitle(book, name)
+  return book.getPageByPath(article.path)
+}
+
+function getAllPeople(book) {
+  return book.summary.getArticleByPath('people.md').articles.map(function(article) {
+    return book.getPageByPath(article.path)
+  })
+}
+
+function getArticleByTitle(book, title) {
+  var path
+  book.summary.walk(function(_article) {
+    if (_article.title == title)
+      path = _article.path
+  })
+  return book.summary.getArticleByPath(path)
+}
+
 module.exports = {
   filters: {
-    // return a new, filtered Array of articles where the given property matches one of the values
-    oneOf(articles, property, values) {
-      values = values || []
-      return articles.reduce(function(result, article) {
-        if (values.indexOf(article[property]) >= 0) {
-          return result.concat([article])
-        }
-        return result;
-      }, [])
+
+    // return the roles played by a particular person
+    rolesPlayedBy(name) {
+      var book = this
+      var person = getPersonNamed(book, name)
+      var roleTitles = person.roles || []
+      return roleTitles.map(function(title) { return getArticleByTitle(book, title) })
     },
 
-    // find articles with the given title
-    findArticlesUnderTitle: function(title) {
-      var path
-      this.summary.walk(function(_article) {
-        if (_article.title == title)
-          path = _article.path
-      })
-      return this.summary.getArticleByPath(path).articles
+    // return all the people playing a given role
+    peoplePlayingRole(roleTitle) {
+      var book = this
+      return getAllPeople(book).reduce(function(result, person) {
+        var roleTitles = person.roles || []
+        if (roleTitles.indexOf(roleTitle) >= 0)
+          return result.concat(person)
+        return result
+      }, [])
     }
   }
 };
